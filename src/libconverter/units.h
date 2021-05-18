7d552fdde4dbfa3b5680a8a9bd5dc9e985bf4796
Kirill Ryabov <sailor.kirill@gmail.com>
2021-05-18 19:33:35 +0700

    Remove code from files
    The code in these files doesn't work now and is unlikely to be needed,
    since the project struct has slightly changed.

--- src/libconverter/units.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <libconverter/input.h>
-
-DefineUnits* from_one_unit(
-        DefineUnits* units,
-        BSTree* unit_list,
-        int num_of_units,
-        const char* coef_list_path);
