468710ef006e82acb9648e3fb75f9523a68aa5a8
Kirill Ryabov <sailor.kirill@gmail.com>
2021-05-30 20:55:47 +0700

    Add some tests for 'is_alpha' function

---
+++ test/test_check.c
@@ -0,0 +1,35 @@
+#include <libconverter/check.h>
+
+#include <ctest.h>
+
+CTEST(is_alpha, correct_string_1)
+{
+    char* string = "DatA-rAtE";
+    const bool real = is_alpha(string);
+
+    ASSERT_TRUE(real);
+}
+
+CTEST(is_alpha, correct_string_2)
+{
+    char* string = "mBps";
+    const bool real = is_alpha(string);
+
+    ASSERT_TRUE(real);
+}
+
+CTEST(is_alpha, correct_string_3)
+{
+    char* string = "yEAr";
+    const bool real = is_alpha(string);
+
+    ASSERT_TRUE(real);
+}
+
+CTEST(is_alpha, incorrect_string)
+{
+    char* string = "Dat.A-rA2tE";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
