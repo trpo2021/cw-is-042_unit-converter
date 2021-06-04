e21aa269cfd891aaffdec3945d9f28536ee39eb0
Nikita Kolodey <GuriForLove@yandex.ru>
2021-06-04 18:46:27 +0700

    Add tests for category check

--- test/test_check.c
+++ test/test_check.c
@@ -41,3 +41,51 @@ CTEST(is_alpha, incorrect_string_2)
 
     ASSERT_FALSE(real);
 }
+
+CTEST(is_alpha, incorrect_string_3)
+{
+    char* string = "lE+2nGt0Th";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_4)
+{
+    char* string = "Dat2A_s1ze";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_5)
+{
+    char* string = "d4t4-rate";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_6)
+{
+    char* string = "T1me";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_7)
+{
+    char* string = "r4T3";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_8)
+{
+    char* string = "Скорость данных";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
