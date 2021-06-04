453f41024cbe71bc28c4c762107bdc0c06290f3c
Nikita Kolodey <GuriForLove@yandex.ru>
2021-06-04 19:25:16 +0700

    Add test check for Length category

--- test/test_check.c
+++ test/test_check.c
@@ -26,6 +26,15 @@ CTEST(is_alpha, correct_string_3)
     ASSERT_TRUE(real);
 }
 
+CTEST(is_alpha, correct_string_4)
+{
+    char* string = "MiCrOmEtErs";
+    const bool real = is_alpha(string);
+
+    ASSERT_TRUE(real);
+}
+
+
 CTEST(is_alpha, incorrect_string_1)
 {
     char* string = "Dat.A-rA2tE";
@@ -84,7 +93,136 @@ CTEST(is_alpha, incorrect_string_7)
 
 CTEST(is_alpha, incorrect_string_8)
 {
-    char* string = "Скорость данных";
+    char* string = "Km.";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_9)
+{
+    char* string = "k1l0m3t3rs";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_10)
+{
+    char* string = " km";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_11)
+{
+    char* string = "M3t3r$";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real); 
+}
+
+CTEST(is_alpha, incorrect_string_12)
+{
+    char* string = "cm.";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_13)
+{
+    char* string = "C4anT1-m3t3r$";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_14)
+{
+    char* string = "m111i-m_3eters";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_15)
+{
+    char* string = "mm.";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+
+CTEST(is_alpha, incorrect_string_16)
+{
+    char* string = "um.";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_17)
+{
+    char* string = "m1cr0m3tR$";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_18)
+{
+    char* string = "nm/";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_19)
+{
+    char* string = "n m";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_20)
+{
+    char* string = "nanom3t3rs";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_21)
+{
+    char* string = "m1les";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_22)
+{
+    char* string = "miles.";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_23)
+{
+    char* string = "F00t$";
+    const bool real = is_alpha(string);
+
+    ASSERT_FALSE(real);
+}
+
+CTEST(is_alpha, incorrect_string_24)
+{
+    char* string = "ft.";
     const bool real = is_alpha(string);
 
     ASSERT_FALSE(real);
