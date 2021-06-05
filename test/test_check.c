ca1de7de9ecec6ffe174f38eef9e2d9e45aad357
Kirill Ryabov <sailor.kirill@gmail.com>
2021-06-05 20:18:52 +0700

    Add test for 'is_appropriate' func

--- test/test_check.c
+++ test/test_check.c
@@ -1,6 +1,7 @@
 #include <libconverter/check.h>
 
 #include <ctest.h>
+#include <stdlib.h>
 
 CTEST(is_alpha, correct_string_1)
 {
@@ -1024,4 +1025,21 @@ CTEST(check_number_length, incorrect_string_2)
     const int real = check_number_length(string);
     const int exp = -1;
     ASSERT_EQUAL(exp, real);
+}
+
+CTEST(is_appropriate, correct_units_1)
+{
+    DefineUnits* units = malloc(sizeof(DefineUnits*));
+    ASSERT_NOT_NULL(units);
+    units->category = "time";
+    units->have_unit = "day";
+    units->want_unit = "sec";
+
+    ListNode* list;
+    list = data_file_parser();
+    ASSERT_NOT_NULL(list);
+
+    const bool real = is_appropriate(list, units);
+    free(units);
+    ASSERT_TRUE(real);
 }
